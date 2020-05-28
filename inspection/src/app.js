b38a397ac5cea2a986b70a6a82d301c7501a6e73
Sean Wong <wotzhs@gmail.com>
2020-05-28 18:24:19 +0800

    Boilerplate (#1)
    * boilerplate: add eslint and restructure project files
    
    * boilerplate: gitnore dist folders
    
    * boilerplate: add babel transpilers and various related npm run scripts
    
    * boilerplate: add babelrc to all services
    
    * boilerplate: rename www to www.js
    
    * boilerplate: replace all requires to use es6 imports

---
+++ inspection/src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path from "path";
+import logger from "morgan";
+import indexRouter from "./routes/index";
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter);
+
+export default app;
