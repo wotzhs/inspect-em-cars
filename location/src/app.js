1bd980c578805564589098c93c0560e5f23c9e9c
Sean Wong <wotzhs@gmail.com>
2020-05-31 02:14:43 +0800

    availability: activate nats worker in location service

--- location/src/app.js
+++ location/src/app.js
@@ -2,6 +2,8 @@ import grpc from "grpc";
 import { loadSync } from "@grpc/proto-loader";
 import commonGrpcConfig from "../../grpc-common-config";
 import Routes from "./routes";
+import nc from "../clients/nats";
+import NATSWorker from "./workers/nats";
 
 const packageDef = loadSync("location/location.proto", commonGrpcConfig);
 const { LocationService } = grpc.loadPackageDefinition(packageDef).location;
@@ -11,7 +13,11 @@ const server = new grpc.Server();
 server.addService(LocationService.service, {
 	getLocations: Routes.getLocations,
 	getLocationAvailabilities: Routes.getLocationAvailabilities,
+	updateLocationAvailabilities: Routes.updateLocationAvailabilities,
 });
+
+const natsWorker = new NATSWorker(nc);
+natsWorker.init();
 
 const SERVER_IP = process.env.SERVER_IP || "0.0.0.0";
 const SERVER_PORT = process.env.SERVER_PORT || "50051";
